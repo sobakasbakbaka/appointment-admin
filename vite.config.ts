cf12234c5eeccf8dc4bd1b3b6f61536abfcc64ad
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-11-24 04:48:22 +0500

    try fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.VITE_FRONTEND_PORT) || 3000,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL,
