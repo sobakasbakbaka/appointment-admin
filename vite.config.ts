94516fd2ec126169b5c51c16871ec5f32f3846e0
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-11-24 04:09:33 +0500

    edited vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
-  },
   server: {
+    port: Number(process.env.VITE_FRONTEND_PORT) || 3000,
     proxy: {
-      "/api": process.env.VITE_API_URL || "http://localhost:5000",
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
