ca88c03e73afeafb866807bfc03dac207e1d594b
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2024-11-24 22:43:47 +0500

    edited vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
